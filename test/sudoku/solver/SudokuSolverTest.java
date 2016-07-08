d2e3751fe0123ec82cac814f89c344e1947b6e30
robson.alecio <robson@thunderbees.com>
2016-07-08 14:35:42 -0300

    Preparando para os testes

---
+++ test/sudoku/solver/SudokuSolverTest.java
@@ -0,0 +1,14 @@
+package sudoku.solver;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SudokuSolverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
